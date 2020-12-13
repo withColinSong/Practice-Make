4afb5100aa5118ff168ceae023e9307ed8385d92
withColinSong <withcolinsong@gmail.com>
2020-12-13 20:23:33 +0900

    회원가입 시 유저 정보 class

--- 회원가입/login_jsp_version/src/user/User.java
+++ 회원가입/login_jsp_version/src/user/User.java
@@ -4,4 +4,23 @@ public class User {
 	private String UserId;
 	private String password;
 	private String email;
+
+	
+	public User(String userId, String password, String email) {
+		super();
+		this.UserId = userId;
+		this.password = password;
+		this.email = email;
+	}
+	
+	public String getUserId() {
+		return UserId;
+	}
+
+	@Override
+	public String toString() {
+		return "User [UserId=" + UserId + ", password=" + password + ", email=" + email + "]";
+	}
+	
+	
 }
