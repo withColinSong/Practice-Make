dc0ae14d5d363b1d87e0498dbb2485f1650648fa
withColinSong <withcolinsong@gmail.com>
2021-01-03 18:23:46 +0900

    login_jsp_version DB

---
+++ 회원가입/login_jsp_version/DB.sql
@@ -0,0 +1,11 @@
+CREATE TABLE usersJoin(
+    mid VARCHAR2(20),
+    pw VARCHAR2(20),
+    email VARCHAR2(20)
+);
+
+select * from usersJoin;
+
+INSERT INTO usersJoin 
+VALUES('song', 'song12', 'song@gmail.com');
+
